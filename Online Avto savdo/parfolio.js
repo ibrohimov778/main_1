06b23204bb5e748aa40bb3c41d5bf38e52a5a4d6
ibrohimov778 <sinyorstudent@gmail.com>
2024-07-29 16:42:48 +0300

    Anor Market

---
+++ Online Avto savdo/parfolio.js
@@ -0,0 +1,14 @@
+let malumotlar=[]
+let savollar=[
+   " Ismingiz",
+   " Yoshingiz",
+   "Millatingiz",
+   "Tel nomeringiz",
+
+
+]
+for(let i=0; i<savollar.length; i++){
+    malumotlar[i]=prompt(savollar[i])
+
+}
+console.log(malumotlar)
